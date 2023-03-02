c906ba0e19408c0bf77fa61d56f1166cb61e8d5d
andre.lgpinto <andregomeees0@gmail.com>
2023-03-01 21:54:00 -0300

    Criando um repositório para projeto compras

---
+++ UC9_COMPRAS/Program.cs
@@ -0,0 +1,11 @@
+﻿// Criar variáveis necessarias
+int i;
+
+
+double produto, total;
+for (i = 1; i <= 5; i++)
+{
+    Console.WriteLine("informe o " + i "° valor de produto");
+    produto = double.Parse(Console.ReadLine());
+    total = total + produto;
+}
